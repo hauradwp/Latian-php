77277c1d0c5d7795984561b2f2a0efb1e92ca6af
hauradwp <hauradwika>
2020-11-13 09:11:57 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+$akun = [["username"=>"Haura Dwika", "password"=>"hauuu"]];
+if (isset($_POST["submit"])) {
+	$cekemail = FALSE;
+	foreach ($akun as $account) {
+	    if ($account["username"] == $_POST["username"]) {
+	        if ($account['password'] == $_POST['password']) {
+	            echo 'Login berhasil!<br><br>';
+	        } else {
+	            echo 'Password salah<br><br>';
+	        }
+	        $cekemail = TRUE;
+	        break;
+	    }
+	}
+	if ($cekemail === FALSE) {
+	    echo 'Username salah<br><br>';
+	}
+}
